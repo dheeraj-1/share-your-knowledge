a74a8de471014e00bb8a21746849a12a48e12659
nagsalesforcedev <dgsalesforcedev@nagarro.com>
2020-08-11 13:45:20 +0530

    Updates

---
+++ precache-manifest.b68403cceef409053591ba7abf8898a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2bf1edfb12c24bd09d5248c1af37c03",
+    "url": "/share-your-knowledge/index.html"
+  },
+  {
+    "revision": "fc1f8200b55bf3c6a749",
+    "url": "/share-your-knowledge/static/css/main.5d74282f.chunk.css"
+  },
+  {
+    "revision": "5dbdb695c61c69b74d1b",
+    "url": "/share-your-knowledge/static/js/2.b51667ec.chunk.js"
+  },
+  {
+    "revision": "46f91ec18259a53c6f750cd89192fcc8",
+    "url": "/share-your-knowledge/static/js/2.b51667ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc1f8200b55bf3c6a749",
+    "url": "/share-your-knowledge/static/js/main.7060e27d.chunk.js"
+  },
+  {
+    "revision": "f187bd7a892d7fc593ea",
+    "url": "/share-your-knowledge/static/js/runtime-main.2aca1a7f.js"
+  }
+]);
